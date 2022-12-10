e6bd67cdd39522e0e580b6212d58b97ff5838960
yashkapoor71 <113561170+yashkapoor71@users.noreply.github.com>
2022-12-09 23:58:25 -0500

    Update code1.java

--- code1.java
+++ code1.java
@@ -1,38 +1,27 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class LabProgram {
+   
+   public static int fibonacci(int n) {
+      if(n<0) {return -1;}
+
+int t1=0,t2=1;
+int nextTerm=0;
+for (int i = 1; i<= n; ++i)
+{
+nextTerm = t1 + t2;
+t1 = t2;
+t2 = nextTerm;
+
+}
+return t1;
+}      
+   
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int[] userValues = new int[20];   // List of integers from input
-
-      {
-	    Scanner sc = new Scanner(System.in);
-	    int len, i;
-	    // take input length
-	    len = sc.nextInt();
-	    // take input array
-	    int arr[] = new int[len];
-	    for(i=0; i<len; i++)
-	    arr[i] = sc.nextInt();
-	    int low=0, high=len-1;
-	    boolean flag = true;
-	    // check from both ends
-	    while(low < high)
-	    {
-	        // if any symmetric value is different, it is not a palindrome
-	        if(arr[low] != arr[high])
-	        {
-	            flag = false;
-	            break;
-	        }
-	        low++;
-	        high--;
-	    }
-	    // display result
-	    if(flag)
-	    System.out.println("yes");
-	    else
-	    System.out.println("no");
-      }
+      int startNum;
+      
+      startNum = scnr.nextInt();
+      System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
    }
 }
